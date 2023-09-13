83acc2b1e6d7780d3ce066755d609cf322f046d7
Seonghui Moon <seonghmo@c1r6s4.42seoul.kr>
2023-09-13 18:51:40 +0900

    norm

--- pipex/main.c
+++ pipex/main.c
@@ -3,59 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: moonseonghui <moonseonghui@student.42.f    +#+  +:+       +#+        */
+/*   By: seonghmo <seonghmo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 15:41:19 by moonseonghu       #+#    #+#             */
-/*   Updated: 2023/09/13 17:07:33 by moonseonghu      ###   ########.fr       */
+/*   Updated: 2023/09/13 17:26:01 by seonghmo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char **get_path(char **envp){
-    int i;
-    char **path;
+char	**get_path(char **envp)
+{
+	int		i;
+	char	**path;
 
-    i = 0;
-    while(envp[i])
-    {
-        if(ft_strncmp("PATH" ,envp[i], 4) == 0)
-        {
-            path = ft_split(envp[i] + 5, ':');  
-            return(path);
-        }
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp("PATH", envp[i], 4) == 0)
+		{
+			path = ft_split(envp[i] + 5, ':');
+			return (path);
+		}
+		i++;
+	}
+	return (0);
 }
 
-char *get_cmd(char **path, char *cmd)
+char	*get_cmd(char **path, char *cmd)
 {
-    int i;
-    char *tmp_cmd;
-    int fd;
-    char *tmp;
-    
-    i = 0;
-    fd = access(cmd, X_OK);
-    if (fd != -1)
-        return (cmd);
-    tmp_cmd = ft_strjoin("/", cmd);
-    while (path[i])
-    {
-       tmp = ft_strjoin(path[i], tmp_cmd);
-       fd = access(tmp, X_OK);
-       if (fd != -1)
-        {
-            free(tmp_cmd);
-            return (tmp);
-        }
-        close(fd);
-        free(tmp);
-        i++;
-    }
-    free(tmp_cmd);
-    return (0);
+	int		i;
+	char	*tmp_cmd;
+	int		fd;
+	char	*tmp;
+
+	i = 0;
+	fd = access(cmd, X_OK);
+	if (fd != -1)
+		return (cmd);
+	tmp_cmd = ft_strjoin("/", cmd);
+	while (path[i])
+	{
+		tmp = ft_strjoin(path[i], tmp_cmd);
+		fd = access(tmp, X_OK);
+		if (fd != -1)
+		{
+			free(tmp_cmd);
+			return (tmp);
+		}
+		close(fd);
+		free(tmp);
+		i++;
+	}
+	free(tmp_cmd);
+	return (0);
 }
 
 int	main(int ac, char **av, char **envp)
@@ -63,26 +64,21 @@ int	main(int ac, char **av, char **envp)
 	t_arg	arg;
 
 	if (ac != 5)
-     write(1, "error\n", 6);
-	
+		write (1, "error\n", 6);
 	arg.infile = open(av[1], O_RDONLY);
 	if (arg.infile == -1)
-		 write(1, "error1\n", 7);
+		write (1, "error1\n", 7);
 	arg.outfile = open(av[4], O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (arg.outfile == -1)
-		 write(1, "error2\n", 7);
-	
+		write (1, "error2\n", 7);
 	arg.path = get_path(envp);
-
 	arg.cmd1_str = ft_split(av[2], ' ');
 	arg.cmd2_str = ft_split(av[3], ' ');
 	if (arg.cmd1_str == NULL || arg.cmd2_str == NULL)
-		 write(1, "error\n", 6);
+		write(1, "error\n", 6);
 	arg.cmd1 = get_cmd(arg.path, arg.cmd1_str[0]);
 	arg.cmd2 = get_cmd(arg.path, arg.cmd2_str[0]);
 	if (arg.cmd1 == NULL || arg.cmd2 == NULL)
-		 write(1, "error\n", 6);
-   
-	//printf("%s %s\n", arg.cmd1, arg.cmd1_str[1]);	
-     pipex(ac, arg, envp);
+		write(1, "error\n", 6);
+	pipex(ac, arg, envp);
 }
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: moonseonghui <moonseonghui@student.42.f    +#+  +:+       +#+        */
+/*   By: seonghmo <seonghmo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 15:42:43 by moonseonghu       #+#    #+#             */
-/*   Updated: 2023/09/13 17:07:37 by moonseonghu      ###   ########.fr       */
+/*   Updated: 2023/09/13 17:29:15 by seonghmo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,24 @@
 # define PIPEX_H
 # include <unistd.h>
 # include <stdlib.h>
-#include <fcntl.h>
-#include <sys/wait.h>
-#include <stdio.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <stdio.h>
 
 typedef struct s_arg
 {
-    int infile;         
-    int outfile;        
-    char **path;        
-    char **cmd1_str;    // 첫 번째 명령어와 인자 배열
-    char **cmd2_str;    // 두 번째 명령어와 인자 배열
-    char *cmd1;         // 실행 가능한 첫 번째 명령어 경로
-    char *cmd2;         // 실행 가능한 두 번째 명령어 경로
-    
+	int		infile;
+	int		outfile;
+	char	**path;
+	char	**cmd1_str;
+	char	**cmd2_str;
+	char	*cmd1;
+	char	*cmd2;
 }t_arg;
 
 char	**ft_split(char const *s, char c);
-int	ft_strncmp(char *s1, const char *s2, int n);
+int		ft_strncmp(char *s1, const char *s2, int n);
 char	*ft_strjoin(char *s1, char *s2);
-void pipex(int ac, t_arg arg, char **envp);
+void	pipex(int ac, t_arg arg, char **envp);
 
 #endif
