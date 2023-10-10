3de61fe6cd03154dea882ffaa3488fb6b6d17bb0
Seonghui Moon <seonghmo@c10r1s2.42seoul.kr>
2023-10-10 14:07:20 +0900

    so_long norm

--- so_long/checking_map2.c
+++ so_long/checking_map2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   checking_map2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: moonseonghui <moonseonghui@student.42.f    +#+  +:+       +#+        */
+/*   By: seonghmo <seonghmo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 19:58:42 by moonseonghu       #+#    #+#             */
-/*   Updated: 2023/10/09 21:48:28 by moonseonghu      ###   ########.fr       */
+/*   Updated: 2023/10/10 14:06:35 by seonghmo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ int	init_check(t_check *check, t_game *game)
 	check->c = 0;
 	if (game->map_str)
 	{
-		check->visited = (int *)malloc(sizeof(int) * (ft_strlen(game->map_str)));
+		check->visited
+			= (int *)malloc(sizeof(int) * (ft_strlen(game->map_str)));
 		if (!check->visited)
 			exit(1);
 		ft_memset(check->visited, 0, sizeof(int) * (ft_strlen(game->map_str)));
