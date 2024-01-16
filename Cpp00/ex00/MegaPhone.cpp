07190a6d08e2308c08edbee6d29e490f02b37155
moonseonghui <msh9980@naver.com>
2024-01-16 20:07:53 +0900

    [Cpp00] ex00

---
+++ Cpp00/ex00/MegaPhone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(int ac, char **av) {
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else {
+        for (int i = 1; i < ac; i++) {
+            for (int j = 0; av[i][j]; j++) {
+                if (std::isalpha(av[i][j]))
+                    std::cout << static_cast<char>(std::toupper(av[i][j]));
+                else
+                    std::cout << av[i][j];
+            }
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
