807b4f5f9d6d0a28a8abd4a56395e5674b469220
moonseonghui <msh9980@naver.com>
2024-10-07 16:13:02 +0900

    [FEAT] cpp04 ex03 cure class 구현

---
+++ Cpp04/ex03/Cure.cpp
@@ -0,0 +1,33 @@
+#include "Cure.hpp"
+
+Cure::Cure(){
+    type = "Cure";
+    std::cout << type << " constructor created!" << std::endl;
+}
+
+Cure::Cure(const Cure &a){
+    type = a.getType();
+    std::cout << type << "Copy constructor called" << std::endl;
+}
+
+Cure& Cure::operator=(const Cure &a){
+    if(this != &a){
+        type = a.getType();
+    }
+    std::cout << type << "Copy assignment operator called" << std::endl;
+
+    return *this;
+}
+
+Cure::~Cure(){
+    std::cout << "Cure Destructor called" << std::endl;
+}
+
+AMateria* Cure::clone() const{
+    return new Cure();
+    std::cout << "Cure constructor cloned" << std::endl;
+}
+
+void Cure::use(ICharacter& target){
+    std::cout << "* heals " << target.getName() << "’s wounds *"<< std::endl;
+}
