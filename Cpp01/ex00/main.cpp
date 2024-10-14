9dd00a703e665fe647c6ef4e588f62130573ba9c
moonseonghui <msh9980@naver.com>
2024-10-14 16:39:23 +0900

    [FIX] cpp01 ex00 main 수정

--- Cpp01/ex00/main.cpp
+++ Cpp01/ex00/main.cpp
@@ -3,12 +3,15 @@
 int main(){
     Zombie unnamedZombie;
 	unnamedZombie.announce();
+	std::cout << std::endl;
 
     Zombie zombie("stackZombie");
 	zombie.announce();
+	std::cout << std::endl;
 
 	randomChump("randomStackZombie");
-
+	std::cout << std::endl;
+	
 	Zombie *newZom = newZombie("heapZombie");
 	newZom->announce();
 	delete(newZom);
